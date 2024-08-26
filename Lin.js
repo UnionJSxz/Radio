f5700de02c1a606feedc9b22e2dd1b7b1e507a17
KING.JS <48418774+UnionJSxz@users.noreply.github.com>
2024-08-26 10:28:04 -0400

    Atualizar o Lin.js

--- Lin.js
+++ Lin.js
@@ -2,7 +2,7 @@ window.onload = function(){
 
 var chave = localStorage.getItem("ngIdle.expiry");
 
-localStorage.removeItem("ngIdle.expiry")
+localStorage.removeItem("ngIdle.expiry");
 
 var newMonth = "05";
 var newChave = chave.substring(0, 32) + newMonth + chave.substring(34);
