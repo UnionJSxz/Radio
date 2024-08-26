8652149ae5743235bf547a233e0967c60850af5d
KING.JS <48418774+UnionJSxz@users.noreply.github.com>
2024-08-26 08:30:48 -0400

    Atualizar o Lin.js

--- Lin.js
+++ Lin.js
@@ -1,40 +1,13 @@
-window.onload = function() {
-    // Nome da chave original
-    let chaveOriginal = 'ngIdle.expiry';
+window.onload = function(){
 
-    // Recuperar a string JSON do localStorage
-    let str = localStorage.getItem(chaveOriginal);
+var chave = localStorage.getItem("ngIdle.expiry");
 
-    if (str !== null) {
-        // Converter a string JSON em um objeto JavaScript
-        let obj = JSON.parse(str);
+localStorage.removeItem("ngIdle.expiry")
 
-        // Extrair a data original
-        let dataOriginal = obj.time;
+var newMonth = "05";
+var newChave = chave.substring(0, 32) + newMonth + chave.substring(34);
 
-        // Verificar se a data está no formato esperado e modificar o mês para "05"
-        let novaData = dataOriginal.replace(/-\d{2}(?=T)/, '-05');
+localStorage.setItem("ngIdle.expiry",newChave)
 
-        // Atualizar o objeto com a nova data
-        obj.time = novaData;
-
-        // Converter o objeto de volta para uma string JSON
-        let novaStr = JSON.stringify(obj);
-
-        // Remover a chave original
-        localStorage.removeItem(chaveOriginal);
-
-        // Criar a chave original novamente com o valor atualizado
-        localStorage.setItem(chaveOriginal, novaStr);
-
-        console.log('Chave atualizada:');
-
-        // Alterar o título após 9 segundos
-        setTimeout(() => {
-            document.title = 'TOTVS Manufatura(Limited)';
-            console.log('Título alterado para TOTVS Manufatura(Limited)');
-        }, 9000); // 9000 milissegundos = 9 segundos
-    } else {
-        console.log('Chave não encontrada.');
-    }
-};
+setTimeout(()=>{document.title="TOTVS Manufatura(Limited)"},8000)
+}
