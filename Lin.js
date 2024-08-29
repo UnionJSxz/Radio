a61871b18f4b862ff9d713542e6423ce058bab61
KING.JS <48418774+UnionJSxz@users.noreply.github.com>
2024-08-29 19:35:52 -0400

    Atualizar o Lin.js

--- Lin.js
+++ Lin.js
@@ -1,11 +1,5 @@
-
 var chave = localStorage.getItem("ngIdle.expiry");
-
 localStorage.removeItem("ngIdle.expiry");
-
-var newMonth = "05";
-var newChave = chave.substring(0, 32) + newMonth + chave.substring(34);
-
+var newChave = chave.replace('-08-','-05-')
 localStorage.setItem("ngIdle.expiry",newChave)
-
 setTimeout(()=>{document.title="TOTVS Manufatura(Limited)"},8000)
